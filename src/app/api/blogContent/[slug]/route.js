7c8f82b0ea5fb2fdf2fe0c5d7c6d879d01534dd2
Oliulla <razasm29@gmail.com>
2024-02-12 13:27:30 +0600

    :fire: Remove prev file system for api and :hammer: change get single blog logic

--- src/app/api/blogContent/[slug]/route.js
+++ src/app/api/blogContent/[slug]/route.js
@@ -3,8 +3,9 @@ import { NextResponse } from "next/server";
 import blogContent from "../../../../models/blogContentFile";
 
 export async function PUT(request, { params }) {
-  const { id } = params;
+  const { slug } = params;
   const { blogTitle, metaTitle, metaDescription, metaKeywords, shortDescription, content } = await request.json();
+  const id = slug;
   await connectMongoDB();
   const blogList = await blogContent.findByIdAndUpdate(id, {
     blogTitle,
@@ -18,8 +19,13 @@ export async function PUT(request, { params }) {
 }
 
 export async function GET(request, { params }) {
-  const { metaTitle } = params;
+  const { slug } = params;
   // const {metaTitle}
+
+  const metaTitle = slug;
+
+  console.log("got metatitle", metaTitle)
+
 
   await connectMongoDB();
   const blogDetailsData = await blogContent.findOne({ metaTitle });
