5a5309a1f5ba6a3fe68ae6ed967ee00a0d68b7ca
Oliulla <razasm29@gmail.com>
2024-02-17 17:10:14 +0600

    :fire: Change bloglist title style

--- src/components/blogsComponent/BlogList/BlogList.js
+++ src/components/blogsComponent/BlogList/BlogList.js
@@ -52,7 +52,7 @@ export default function BlogList({ allBlogList }) {
                 >
                   <th
                     scope="row"
-                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    className="px-6 py-4 font-medium"
                   >
                     {item?.blogTitle}
                   </th>
